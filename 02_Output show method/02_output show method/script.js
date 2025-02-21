96b2227c7ac8b3755f49cb9e665b5eacbfed3afc
pawan2505 <pawanaktu@gmail.com>
2025-02-21 13:31:51 +0530

    JavaScript

---
+++ 02_Output show method/02_output show method/script.js
@@ -0,0 +1,49 @@
+
+// console me run hota
+
+// console.log("Hello Bhai")
+
+// ui pr run hota hai
+
+// document.write("Hello Bhai, kaise ho")
+
+// writeln is same as write method
+
+// document.writeln("Hello Bhai, kaise ho");
+// document.writeln("Hello Bhai, kaise ho");
+
+
+// alert("Attension")  // pop-up
+
+
+// document.write(`Hello Bhai, kaise ho <br/>`);
+// document.write(`Hello Bhai, kaise ho <br/>`);
+
+
+
+// variable -> variable is name of memory location where we can store data
+
+// datatype
+
+//  we do not use data type like c,cpp or other programming to declared variable
+// but we use datatype keyword to declared any variable in javascript -> var(es5), let(es6),const(es6)
+
+// let firstName = "Ram"
+
+// console.log(firstName)
+// console.log(typeof firstName);
+
+// let num = 10;
+
+// console.log(num)
+// console.log(typeof num);
+
+// let name = "Pawan"
+
+// document.getElementById('para').innerHTML = `<i>Name : ${name}</i>`;
+
+// document.write("<h2>Welcome to JavaScript</h2>");
+// document.write("<p>This is a paragraph written using JavaScript.</p>");
+
+// let userName = "Amit";
+// alert("Hello, " + userName + "! Welcome to our site.");
