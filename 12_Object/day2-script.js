77b04bcf91ebe332d55a46eed326f9e24ef21f0b
pawan2505 <pawanaktu@gmail.com>
2025-03-27 19:50:01 +0530

    object

--- 12_Object/day2-script.js
+++ 12_Object/day2-script.js
@@ -1,87 +1,91 @@
-// let obj = {
-//   name: "Pawan",
-//     age: 25,
-//     subject: ["HTML", "CSS", "JS"],
-  
-//     car: {
-//         car1: "BMW",
-//         car2:"Mahindra"
-//     }
-// };
-
-// console.log(obj);
-
-// obj.salary = 999;
-
-// console.log(obj)
-
-// obj.name = "Poojan"
-
-// console.log(obj);
-
-
-// delete obj.age;
-
-
-// console.log(obj);
-
-// console.log(obj.car.car2)
-
-
-
-// const students = [
-//     { name: "Amit", age: 20 },
-//     { name: "Sara", age: 22 },
-//     { name: "Kunal", age: 21 }
-// ];
-// console.log(students[1].age); // Output: Sara
-
-
-// students.forEach((student) => {
-//   console.log(student.name + " is " + student.age + " years old.");
-// });
-
-// for (let student of students) {
-//   console.log(student.name);
-// }
-
-
+// Example of an object with different properties including a nested object and an array
 let obj = {
-  name: "Pawan",
-    age: 25,
-    subject: ["HTML", "CSS", "JS"],
-  
-    car: {
-        car1: "BMW",
-        car2:"Mahindra"
-    }
+  name: "Pawan", // String property: Name
+  age: 25, // Number property: Age
+  subject: ["HTML", "CSS", "JS"], // Array property: Subjects
+
+  car: {
+    car1: "BMW", // Nested object inside 'car'
+    car2: "Mahindra", // Nested object inside 'car'
+  },
 };
 
+// Output the entire object to the console
+console.log(obj);
 
-// console.log(Object.keys(obj.car))
+// Adding a new property 'salary' to the object
+obj.salary = 999;
 
-// console.log(Object.values(obj.car))
+// Output the updated object to the console
+console.log(obj);
 
-// console.log(Object.entries(obj))
+// Modifying an existing property 'name' in the object
+obj.name = "Poojan";
 
+// Output the object after modification
+console.log(obj);
 
-let obj1 = Object.create(obj);
+// Deleting the 'age' property from the object
+delete obj.age;
 
-console.log(obj1.name)
+// Output the object after deleting the 'age' property
+console.log(obj);
 
-obj1.abc = "dewe" // not ok
+// Accessing a property of a nested object using dot notation
+console.log(obj.car.car2); // Output: Mahindra
 
-console.log(obj)
+// Example of an array of objects (representing students)
+const students = [
+  { name: "Amit", age: 20 }, // Object with 'name' and 'age' properties
+  { name: "Sara", age: 22 },
+  { name: "Kunal", age: 21 },
+];
 
+// Accessing a specific property (age) of the second student (Sara)
+console.log(students[1].age); // Output: 22 (Sara's age)
 
+// Looping over the 'students' array using forEach to print the name and age of each student
+students.forEach((student) => {
+  console.log(student.name + " is " + student.age + " years old.");
+});
 
-obj2 = {
-    name:"Shita",
-    abc: "Efwe",
-    dwe:"wfewe"
+// Another loop using for..of to print only the names of the students
+for (let student of students) {
+  console.log(student.name);
 }
 
-Object.assign(obj, obj2)
+// Example of Object methods
 
-console.log(obj)
+// Accessing the keys of the 'car' object (nested inside 'obj')
+console.log(Object.keys(obj.car)); // Output: ['car1', 'car2']
 
+// Accessing the values of the 'car' object
+console.log(Object.values(obj.car)); // Output: ['BMW', 'Mahindra']
+
+// Accessing both keys and values of the 'obj' object using Object.entries
+console.log(Object.entries(obj)); // Output: [['name', 'Pawan'], ['age', 25], ['subject', ['HTML', 'CSS', 'JS']], ['car', { car1: 'BMW', car2: 'Mahindra' }], ['salary', 999]]
+
+// Creating a new object 'obj1' that inherits from 'obj' using Object.create()
+let obj1 = Object.create(obj);
+
+// Accessing the 'name' property of the inherited object 'obj1'
+console.log(obj1.name); // Output: Pawan (inherited from 'obj')
+
+// Adding a new property to 'obj1' (This is not okay because 'abc' is not part of 'obj1')
+obj1.abc = "dewe"; // This is not a part of obj1
+
+// Displaying 'obj' to show that the original object remains unchanged
+console.log(obj);
+
+// Example of merging properties from one object into another using Object.assign
+let obj2 = {
+  name: "Shita", // New name property
+  abc: "Efwe", // New abc property
+  dwe: "wfewe", // New dwe property
+};
+
+// Merging 'obj2' into 'obj', so properties of 'obj2' will be added to 'obj'
+Object.assign(obj, obj2);
+
+// Displaying the 'obj' after merging the properties of 'obj2'
+console.log(obj); // Output: { name: 'Shita', age: 25, subject: ['HTML', 'CSS', 'JS'], car: { car1: 'BMW', car2: 'Mahindra' }, salary: 999, abc: 'Efwe', dwe: 'wfewe' }
