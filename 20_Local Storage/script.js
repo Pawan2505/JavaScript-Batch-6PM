a60fd89c6c8f3b7e860565b4a577b44e4d6c9aaa
pawan2505 <pawanaktu@gmail.com>
2025-04-09 19:00:25 +0530

    local storage

---
+++ 20_Local Storage/script.js
@@ -0,0 +1,83 @@
+// let name = "Poojan";
+
+// localStorage.setItem("student", name);
+
+// let username = localStorage.getItem("student");
+
+// console.log(username)
+
+// let num = 12;
+
+// localStorage.setItem("student", num);
+
+// let username = localStorage.getItem("student");
+
+// console.log(username);
+
+// console.log(typeof username);
+
+
+// const obj1 = {
+//     id: 1,
+//     usename: "Poojan",
+//     course:"Full Stack"
+// }
+
+// // localStorage.setItem("user", obj1);
+
+// localStorage.setItem("user", JSON.stringify(obj1));
+
+
+// let data = localStorage.getItem("user");
+// console.log(data)
+
+// console.log(typeof data)
+
+// let finalData = JSON.parse(data);
+
+// console.log(finalData)
+
+// console.log(typeof finalData)
+
+// obj1.salary = 235346;
+// localStorage.setItem("user", JSON.stringify(obj1));
+// console.log(obj1)
+
+// let result = localStorage.getItem("user");
+
+// console.log(JSON.parse(result));
+
+
+// obj1.course = "MERN"
+
+// localStorage.setItem("user", JSON.stringify(obj1));
+
+// let updatedata = localStorage.getItem("user");
+
+// console.log(JSON.parse(updatedata))
+
+// delete obj1.salary;
+// localStorage.setItem("user", JSON.stringify(obj1));
+
+// let afterdelete = localStorage.getItem("user");
+
+// console.log(JSON.parse(afterdelete));
+
+
+
+// JSON -> Javascript object notation
+
+// key -> string formate
+// json -> function nhi bana skte
+
+// and all propert of json same as object
+
+
+// let person = {
+//   id: 1,
+//   name: "Pawan", 
+
+// };
+
+
+// localStorage.setItem("jsondata", JSON.stringify(person));
